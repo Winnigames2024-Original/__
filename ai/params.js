942ecc2c315ade445fe181091792f3dd4ebd16e6
link-short <dora.family228@gmail.com>
2025-12-08 18:02:49 +0500

    Update params.js

--- ai/params.js
+++ ai/params.js
@@ -5,10 +5,12 @@ console.log(question);
 
 if (aiModel == "TeleBotAI") {
   const answer = "DON%27T%20BELIVE%20IT";
-  if (question == answer) {
-    alert("ERROR 0x06x5x46. " + "ERROR MESSAGE: " + "!!!DON'T CLOSE ME!!! " + "Current AI Version: " + aiModel + "2x45xfDVD");
+  if (question == "DON%27T%20BELIVE%20IT") {
+    cosnt msg_err = "ERROR 0x06x5x46. " + "ERROR MESSAGE: " + "!!!DON'T CLOSE ME!!! " + "Current AI Version: " + aiModel + "2x45xfDVD";
+    alert(msg_err);
   }
   else {
-    alert("Your question: " + question + ", AI answer: " + answer);
+    cosnt msg_answer = "Your question: " + question + ", AI answer: " + answer;
+    alert(msg_answer);
   }
 }
