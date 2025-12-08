3739aca10f58e4599e168cd50d57d137b2ea506b
link-short <dora.family228@gmail.com>
2025-12-08 18:19:10 +0500

    Update params.js

--- ai/params.js
+++ ai/params.js
@@ -1,17 +1,23 @@
 const urlParams = new URLSearchParams(window.location.search);
 const aiModel = urlParams.get('model');
+const question = urlParams.get('q');
 
 if (aiModel == "TeleBotAI") {
   const answer = "DON%27T%20BELIVE%20IT";
-  const question = urlParams.get('q');
   if (question == "DON%27T%20BELIVE%20IT") {
     cosnt msg_err = "ERROR 0x06x5x46. " + "ERROR MESSAGE: " + "!!!DON'T CLOSE ME!!! " + "Current AI Version: " + aiModel + "-AmandaHackSoftware";
+    
     alert(msg_err);
-    alert("AI: "+ aiModel + " hacked by " + "-AmandaHackSoftware");
-    window.location = "https://winnigames2024-original.github.io/__/ai/?model=AmandaHackSoftware&tool=BesyashiySayt";
+    
+    const msg_err2 = "AI: "+ aiModel + " hacked by " + "-AmandaHackSoftware";
+    
+    alert(msg_err2);
+    
+    window.location = "https://winnigames2024-original.github.io/__/ai/amandasoft.html?model=AmandaHackSoftware&tool=BesyashiySayt";
   }
   else {
     cosnt msg_answer = "Your question: " + question + ", AI answer: " + answer;
+    
     alert(msg_answer);
   }
 }
