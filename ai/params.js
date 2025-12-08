119feaf0f415cd1b5743ab80b4e7887da41978ef
link-short <dora.family228@gmail.com>
2025-12-08 17:54:41 +0500

    Create params.js

---
+++ ai/params.js
@@ -0,0 +1,14 @@
+const urlParams = new URLSearchParams(window.location.search);
+const aiModel = urlParams.get('model');
+const question = urlParams.get('q');
+console.log(question);
+
+if (aiModel == "TeleBotAI") {
+  const answer = "DON'T BELIVE IT";
+  if (question == answer) {
+    alert("ERROR 0x06x5x46. " + "ERROR MESSAGE: " + "!!!DON'T CLOSE ME!!! " + "Current AI Version: " + aiModel + "2x45xfDVD");
+  }
+  else {
+    alert("Your question: " + question + ", AI answer: " + answer);
+  }
+}
